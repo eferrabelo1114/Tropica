2b2be1f6a810ae70211b7952d9b39800896dbdec
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2021-08-16 03:06:14 -0400

    Update CLIENTKnit_Bootstrap.client.lua

--- src/Client/CLIENTKnit_Bootstrap.client.lua
+++ src/Client/CLIENTKnit_Bootstrap.client.lua
@@ -1,5 +1,4 @@
 --Services
-local ContentProvider = game:GetService("ContentProvider")
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local Knit = require(ReplicatedStorage.Knit)
@@ -7,11 +6,5 @@ local Knit = require(ReplicatedStorage.Knit)
 local Player = game.Players.LocalPlayer
 
 Knit.AddControllers(script.Parent:WaitForChild("Controllers"))
-
-
-ContentProvider:PreloadAsync(ReplicatedStorage.Sounds:GetChildren())
-ContentProvider:PreloadAsync(ReplicatedStorage.Animations:GetChildren())
-ContentProvider:PreloadAsync(ReplicatedStorage.Textures:GetChildren())
-
 
 Knit.Start():Catch(warn)
